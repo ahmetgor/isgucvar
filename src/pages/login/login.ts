e27976ea117e456221617fcf6ecef3c78962d94a
ahmetgor <ahmet.gor@yandex.com>
2018-02-26 19:00:07 +0300

    linkedin login devam

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 // import { AuthProvider } from '../../providers/auth';
 import { PersonProvider } from '../../providers/person';
 import { AuthProvider } from '../../providers/auth';
 import { TabsPage } from '../tabs/tabs';
 import { GozatPage } from '../gozat/gozat';
 
-declare var IN;
+declare var IN: any;
+declare var window: any;
 
 @IonicPage()
 @Component({
@@ -16,13 +17,24 @@ declare var IN;
 export class LoginPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public personSer: PersonProvider,
-              public authSer: AuthProvider) {
+              public authSer: AuthProvider, private platform: Platform) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
     // this.doLogin();
     // this.getProfileData();
+  }
+
+  getLinkedPerson() {
+    this.authSer.getLinkedPerson()
+    .then((res) => {
+      console.log("getLinkedPerson cevap")
+      console.log(res);
+
+          }, (err) => {
+            console.log("getLinkedPerson err")
+          });
   }
 
 getProfileData() {
@@ -46,20 +58,65 @@ console.log('getProfileData1');
 
 }
 
+public linkedLogin(): Promise<any> {
+    return new Promise(function(resolve, reject) {
+      var browserRef = window.cordova.InAppBrowser.open("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=" + "86p3aqpfdryb6f" + "&redirect_uri=http://localhost:8080/api/auth/callback&state=252890252890&scope=r_basicprofile", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+        // var browserRef = window.cordova.InAppBrowser.open("https://www.facebook.com/v2.0/dialog/oauth?client_id=" + "CLIENT_ID_HERE" + "&redirect_uri=http://localhost/callback&response_type=token&scope=email", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+        browserRef.addEventListener("loadstart", (event) => {
+        console.log(JSON.stringify(event)+"event");
+        console.log(event.url+"url");
+
+            if ((event.url).indexOf("http://localhost:8080/api/auth/callback") === 0) {
+
+                // browserRef.removeEventListener("exit", (event) => {});
+                // browserRef.close();
+                //
+                // var responseParameters = ((event.url).split("#")[1]).split("&");
+                // console.log(responseParameters+"response");
+                //
+                // var parsedResponse = {};
+                // for (var i = 0; i < responseParameters.length; i++) {
+                //     parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+                // }
+                // console.log(JSON.stringify(parsedResponse)+"parsedres");
+                // if (parsedResponse["access_token"] !== undefined && parsedResponse["access_token"] !== null) {
+                //     resolve(parsedResponse);
+                // } else {
+                //     reject("Problem authenticating with Facebook");
+                // }
+            }
+        });
+        browserRef.addEventListener("exit", function(event) {
+          console.log(JSON.stringify(event)+"exitevent")
+            reject("LinkedIn giriş yapılamadı, lütfen tekrar deneyin");
+        });
+    });
+}
+
 doLogin() {
+
+    this.platform.ready().then(() => {
+        this.linkedLogin().then(success => {
+            alert(success.access_token);
+        }, (error) => {
+            alert(error);
+        });
+    });
+
 
   // this.authSer.login()
   // .then((res) => {
   //   console.log(res);
   //       }, (err) => {
-  //
   // });
-IN.User.authorize(
-  () => {console.log('asd1');
-          this.getProfileData();
-        }
-  , () => console.log('qwe1'));
-}
+
+  // this.authSer.login1();
+
+// IN.User.authorize(
+//   () => {console.log('asd1');
+//           this.getProfileData();
+//         }
+//   , () => console.log('qwe1'));
 
 // doAuth() {
 //   if(IN.User.isAuthorized()){
@@ -82,5 +139,6 @@ IN.User.authorize(
 // function onError(error) {
 //     console.log(error);
 // }
+}
 
 }
--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginPage } from '../pages/login/login';
 import {LoadingController} from 'ionic-angular';
-// import { LinkedIn } from '@ionic-native/linkedin';
+// import { LinkedIn, LinkedInLoginScopes } from '@ionic-native/linkedin';
 
 // import 'rxjs/add/operator/map';
 // import { Observable } from 'rxjs';
@@ -14,9 +14,54 @@ export class AuthProvider {
   loading: any;
   url: string = 'http://127.0.0.1:8080/api/auth/';
   // url : string = 'https://isgucvarserver.herokuapp.com/api/auth/'
+  accessToken: string = 'AQVWMxkd3kzYLGwIOGaeaPCXig9hdCF2JFxNAJfoYaXEECmI1iIRsltRveUoR7-qk-nxnuRDqjdPEEmApDDzMsvylSCmFl9-30lwAvJeGT6DvH40OIGg-TpL-if9d_ANwSjqM7wuJR7JcgYQ8ELjd832UWimD_iWCTVRVnASP2YFE4YCrAp5vgtzQHHAkK5G8TLsJnasZG9YwrNXFVk-Xrz4T6I6nOtVXaQ6X_lis5_ejTyusFi5tD_Q3vlRIAZmo5xN_Soo_s2goavnXDZwYlczU7o5d1BsHyuxowuAyL2kg3Gi9Hhqa4nmc_4BKyojTbAvskmjfXiby2HSaGVA-ks4fi9wlw';
+  linkedUrl: string = "https://api.linkedin.com/v1/people/~:(id,formatted-name,location,industry,summary,specialties"+
+    ",positions,picture-urls::(original),site-standard-profile-request,email-address)?format=json";
 
   constructor(public http: HttpClient, public loadingCtrl: LoadingController) {
     console.log('Hello AuthProvider Provider');
+  }
+
+  getLinkedPerson() {
+    return new Promise((resolve, reject) => {
+      // this.showLoader();
+        let headers = new HttpHeaders();
+        headers.append('Content-Type', 'application/json');
+        // headers.append('Authorization', this.accessToken);
+        // let uri = encodeURI('https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86p3aqpfdryb6f&redirect_uri=http://localhost:8100&state=252890252890&scope=r_basicprofile');
+        this.http.get(this.url+"linkedPerson"+`?token=${this.accessToken}`, {headers: headers})
+          .subscribe(res => {
+            let data = JSON.parse(JSON.stringify(res));
+            console.log(JSON.stringify(data)+'data');
+            // this.loading.dismiss();
+            resolve(data);
+            // resolve(res.json());
+          }, (err) => {
+            // this.loading.dismiss();
+            console.log(JSON.stringify(err)+"err");
+            reject(err);
+          });
+    });
+  }
+
+  doAuth(url: string) {
+    return new Promise((resolve, reject) => {
+      // this.showLoader();
+        let headers = new HttpHeaders();
+        headers.append('Content-Type', 'application/json');
+        // let uri = encodeURI('https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86p3aqpfdryb6f&redirect_uri=http://localhost:8100&state=252890252890&scope=r_basicprofile');
+        this.http.get(url, {headers: headers})
+          .subscribe(res => {
+            let data = JSON.parse(JSON.stringify(res));
+            console.log(JSON.stringify(data)+'data');
+            // this.loading.dismiss();
+            resolve(data);
+            // resolve(res.json());
+          }, (err) => {
+            // this.loading.dismiss();
+            reject(err);
+          });
+    });
   }
 
 login(){
@@ -26,25 +71,27 @@ return new Promise((resolve, reject) => {
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
     // let uri = encodeURI('https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86p3aqpfdryb6f&redirect_uri=http://localhost:8100&state=252890252890&scope=r_basicprofile');
-
     this.http.get(this.url,
       {headers: headers})
       .subscribe(res => {
-
         let data = JSON.parse(JSON.stringify(res));
         console.log(JSON.stringify(data)+'user');
-
         // this.loading.dismiss();
-
         resolve(data);
         // resolve(res.json());
       }, (err) => {
         // this.loading.dismiss();
-
         reject(err);
       });
 });
 }
+
+// login1() {
+//   let scopes = ['r_basicprofile', 'r_emailaddress'];
+// this.linked.login(scopes as LinkedInLoginScopes[], true)
+//   .then(() => console.log('Logged in!'));
+//   // .catch(e => console.log('Error logging in', e));
+// }
 
   showLoader(){
 
