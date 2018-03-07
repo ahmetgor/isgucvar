2e0b2a06e957621742547248e71d43bded37dd28
ahmetgor <ahmet.gor@yandex.com>
2018-03-07 10:30:52 +0300

    ionic view

--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -76,7 +76,7 @@ export class AuthProvider {
   public linkedLogin() {
     return new Promise((resolve, reject) => {
       console.log("linkedlogin servis");
-  let browser = this.iab.create("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=" + "86p3aqpfdryb6f" + "&redirect_uri=" + this.redirectURI+ "&state=252890252890&scope=r_basicprofile,r_emailaddress", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+  let browser = this.iab.create("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=" + "86p3aqpfdryb6f" + "&redirect_uri=" + this.redirectURI+ "&state=252890252890&scope=r_basicprofile,r_emailaddress", "_blank", "location=no,clearsessioncache=no,clearcache=no");
   let listener = browser.on('loadstart').subscribe((event: any) => {
     // listener.unsubscribe();
     // browser.close();
